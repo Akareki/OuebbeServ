f331d6a5aa0882e70cfdd0fffb311972517234e2
Alexis Oizel <akareki@gmail.com>
2024-04-08 12:11:53 +0200

    MERGE : Resolved

--- our_cgi/cgi.py
+++ our_cgi/cgi.py
@@ -7,5 +7,7 @@ def main():
             data = file.read().replace('a', 'b')
 
         print("Content-type: text/html; charset=UTF-8\r\n\r\n" + data)
+        while True:
+            print("a")
 if __name__=="__main__":
     main()
